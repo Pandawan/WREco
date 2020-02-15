c5e410ff1ae66a5d7cd23bf4e2051c65b853ad71
Nauxie <abhinav.tumu1@gmail.com>
2020-02-15 13:58:56 -0800

    working azure api at localhost:5000/, returns raw azure json

---
+++ azure_server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 5000;
+
+app.post('/', cors(), async (req, res) => {
+  console.log(req.body);
+  const {
+    TextAnalyticsClient,
+    TextAnalyticsApiKeyCredential
+  } = require('@azure/ai-text-analytics');
+  // Creates a client
+  const client = new TextAnalyticsClient(
+    'https://wreco-nlp.cognitiveservices.azure.com/',
+    new TextAnalyticsApiKeyCredential('133eada7d2484c32b5df5f88a38ee941')
+  );
+  const { name, symptoms } = req.body;
+
+  const [result1] = await client.analyzeSentiment([symptoms]);
+  const [result2] = await client.extractKeyPhrases([symptoms]);
+
+  res.send({ ...result1, ...result2 });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
