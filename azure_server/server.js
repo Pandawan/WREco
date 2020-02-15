c44275960901b0154ec533d2cfd4ea145b9ef933
Nauxie <abhinav.tumu1@gmail.com>
2020-02-15 14:41:32 -0800

    uses post body to perform keyword/sentiment analysis

--- azure_server/server.js
+++ azure_server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+app.use(express.json({ extended: false }));
 
 app.post('/', cors(), async (req, res) => {
   console.log(req.body);
