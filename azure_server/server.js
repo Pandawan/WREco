fbfd36f1888f8a58f0bcf85354acf5dbdd31dae7
Nauxie <abhinav.tumu1@gmail.com>
2020-02-15 16:36:41 -0800

    working batch files

--- azure_server/server.js
+++ azure_server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+const severities = require('./severity');
 app.use(express.json({ extended: false }));
 
 app.post('/', cors(), async (req, res) => {
   console.log(req.body);
+  console.log(severities);
   const {
     TextAnalyticsClient,
     TextAnalyticsApiKeyCredential
@@ -16,10 +18,13 @@ app.post('/', cors(), async (req, res) => {
     new TextAnalyticsApiKeyCredential('133eada7d2484c32b5df5f88a38ee941')
   );
   const { name, symptoms } = req.body;
+  let returnObj = {};
 
   const [result1] = await client.analyzeSentiment([symptoms]);
   const [result2] = await client.extractKeyPhrases([symptoms]);
-
+  for (let i = 0; i < result2.keyPhrases.length; i++) {
+    for (let j = 0; j < Object.keys(severities); j++) {}
+  }
   res.send({ ...result1, ...result2 });
 });
 
