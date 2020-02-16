81f35db674ed5e9edafb1a267111519ea980a28b
Nauxie <abhinav.tumu1@gmail.com>
2020-02-15 21:57:53 -0800

    Merge UwU

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -5,23 +5,27 @@ import InformationView from 'views/informationView';
 import MainView from 'views/mainView';
 import ResponseView from 'views/responseView';
 import Navbar from 'components/navbar';
+import InfoView from 'views/infoView';
 
 function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <header>
         <Navbar />
       </header>
       <main>
         <BrowserRouter>
           <Switch>
-            <Route path="/response">
+            <Route path='/response'>
               <ResponseView />
             </Route>
-            <Route path="/information">
+            <Route path='/information'>
               <InformationView />
             </Route>
-            <Route path="/">
+            <Route path='/info'>
+              <InfoView />
+            </Route>
+            <Route path='/'>
               <MainView />
             </Route>
           </Switch>
