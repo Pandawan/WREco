1ac243e26060e2b54cb4ea4c2ac3852f6a0ab233
Nauxie <abhinav.tumu1@gmail.com>
2020-02-16 09:41:49 -0800

    data adds

--- web/src/helpers/defaultQueue.js
+++ web/src/helpers/defaultQueue.js
@@ -1,9 +1,32 @@
-
 export const defaultQueue = [
   {
-    name: "Toby",
-    symptoms: [ "strong", "Migraine" ],
-    timestamp: "2020-02-16T06:02:30.207Z",
-    severity: "moderate"
+    name: 'Toby',
+    symptoms: ['strong', 'Migraine'],
+    timestamp: '2020-02-16T06:02:30.207Z',
+    severity: 'moderate'
+  },
+  {
+    name: 'Bill',
+    symptoms: ['strong', 'headache'],
+    timestamp: '2020-02-16T17:28:20.137Z',
+    severity: 'mild'
+  },
+  {
+    name: 'Gabe',
+    symptoms: ['car crash'],
+    timestamp: '2020-02-16T17:28:31.137Z',
+    severity: 'critical'
+  },
+  {
+    name: 'Jefferson',
+    symptoms: ['hives'],
+    timestamp: '2020-02-16T17:28:38.137Z',
+    severity: 'moderate'
+  },
+  {
+    name: 'Stacy',
+    symptoms: ['flu'],
+    timestamp: '2020-02-16T17:28:40.137Z',
+    severity: 'mild'
   }
 ];
